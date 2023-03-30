525d40f6e13755f16f491f05d495ef5bfb250009
sol-cryptokid <thomas.bazire@icloud.com>
2023-03-30 16:51:13 +0200

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,181 @@
 import Wallet from "./Wallet";
 import Transfer from "./Transfer";
-import "./App.scss";
-import { useState } from "react";
+import "./App.css";
+import { useEffect, useState } from "react";
+import server from "./server";
+import { keccak256 } from 'ethereum-cryptography/keccak'
+import { utf8ToBytes, toHex } from 'ethereum-cryptography/utils'
+import {sign} from 'ethereum-cryptography/secp256k1'
+
+function hashMessage (message) {
+  return keccak256(utf8ToBytes(message))
+}
+
+async function signMessage (message, privateKey) {
+  const hash = hashMessage(message)
+  return await sign(hash, privateKey, { recovered: true })
+}
+ 
+function Select ({ options, id, value, onChange, label, inactive }) {
+  return (
+    <>
+      <label htmlFor={id} className="text-sm font-semibold text-white">{ label }</label>
+      <select
+        id={id}
+        value={value}
+        onChange={onChange}
+        className="bg-slate-900 border border-slate-500 text-gray-100 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mb-2"
+      >
+        <option></option>
+        { options && options.map((val, key) => {
+            if (val.privateKey !== inactive) {
+              return <option value={val.privateKey} key={key}>{ val.publicKey } ({ val.balance })</option>
+            }
+          })
+        }
+      </select>
+    </>
+  )
+}
+
+function Input ({ id, value, type, onChange, max }) {
+  return (
+    <input
+      type={type}
+      id={id}
+      value={value}
+      onChange={onChange}
+      min={0}
+      max={max}
+      className="bg-slate-900 border border-slate-500 text-gray-100 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 my-2"
+    />
+  )
+}
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState("");
 
+  const [sender, setSender] = useState()
+  const [recipient, setRecipient] = useState()
+  const [amount, setAmount] = useState(0)
+
+
+  const [wallets, setWallets] = useState([])
+
+  const loadWallets = async () => {
+    const { data } = await server.get('/wallets');
+    setWallets(data)
+    setIsLoading(false)
+  }
+
+  const handleAmount = (e) => {
+    const amount = e?.target?.value
+    const senderBalance = _getPublicInfoFromAddress(sender)?.balance
+    if (sender && amount > senderBalance) {
+      console.error(`Amount requested is too high.`)
+      return false
+    }
+    setAmount(parseInt(amount))
+  }
+
+  const submit = async (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+
+    const recipientPublicKey = _getPublicInfoFromAddress(recipient)?.address
+
+    const [signature, recoveryBit] = await signMessage(`${recipientPublicKey}${amount}`, sender)
+
+    console.log(`recipient: ${recipientPublicKey}. amount: ${amount}. signature: ${signature}`)
+
+    const { data } = await server.post('/send', {
+      recipient: recipientPublicKey,
+      amount,
+      signature,
+      recoveryBit
+    })
+    console.log(data)
+  }
+
+  const _getPublicInfoFromAddress = (addr) => {
+    for (let x = 0; x < wallets.length; x++) {
+      if (addr === wallets[x].privateKey) {
+        return { address: wallets[x].publicKey, balance: wallets[x].balance }
+      }
+    }
+  }
+
+  useEffect(() => {
+    loadWallets()
+  }, [])
+
+  if (isLoading) return <></>
+
+  const senderInfo = _getPublicInfoFromAddress(sender)
+  const recipientInfo = _getPublicInfoFromAddress(recipient)
+
+  return (
+    <div className="bg-slate-950 flex">
+      <div className="w-1/4 bg-slate-700 md:w-1/2 h-auto mx-auto my-10 rounded-xl py-7 px-8">
+        <h1 className="text-lg font-semibold text-white mb-6">Transfering AU funds</h1>
+
+        <Select
+          id="sender"
+          options={wallets}
+          onChange={e => {
+            setSender(e?.target?.value)
+            console.log(`set sender privateKey: (${e?.target?.value})`)
+          }}
+          inactive={recipient}
+          label="Select a sender"
+        />
+
+        <Select
+          id="recipient"
+          options={wallets}
+          onChange={e => setRecipient(e?.target?.value)}
+          inactive={sender}
+          label="Select a recipient"
+        />
+
+        <div className="my-6">
+          <label className="text-sm font-semibold text-white">How much would you send?</label>
+          <Input id="amount" type="number" max={100} onChange={handleAmount} />
+        </div>
+
+
+        { (sender && recipient && amount > 0) &&
+          (
+            <>
+              <hr className="border-slate-500 mt-5" />
+              <div className="preview py-5">
+                <label className="text-sm font-semibold text-white">Transaction preview</label>
+                <div className="text-xs py-2">
+                  <span className="block font-semibold text-gray-300">Sender: {senderInfo?.address}</span>
+                  <span className="block text-gray-400">Actual balance: { senderInfo?.balance } → New balance: { senderInfo?.balance - amount }</span>
+                </div>
+
+                <div className="text-xs py-2">
+                  <span className="block font-semibold text-gray-300">Recipient: {recipientInfo?.address}</span>
+                  <span className="block text-gray-400">Actual balance: { recipientInfo?.balance } → New balance: { recipientInfo?.balance + amount }</span>
+                </div>
+              </div>
+            </>
+          )
+        }
+
+        <button className="w-full block rounded-lg text-sm font-semibold py-3.5 bg-emerald-600 hover:bg-emerald-500 text-gray-200 hover:text-white transition shadow-sm text-shadow" onClick={submit}>Send funds</button>
+      </div>
+    </div>
+  )
+
   return (
     <div className="app">
+      <div className="container">
+        Addresses
+      </div>
       <Wallet
         balance={balance}
         setBalance={setBalance}
--- server/index.js
+++ server/index.js
@@ -1,16 +1,68 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { keccak256 } = require('ethereum-cryptography/keccak');
+const { utf8ToBytes } = require('ethereum-cryptography/utils');
+const { sign, recoverPublicKey } = require('ethereum-cryptography/secp256k1');
 const port = 3042;
 
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
-};
+const wallets = [
+  {
+    privateKey: '11e44159a41ec9f208e084379e763fb1c3c889f617b3d5ff481e46ac6f0308da',
+    publicKey: '0x46f1074fdd6d81a4792d',
+    balance: 100
+  },
+  {
+    privateKey: '42ddb45bcf40ec11ae3b9c8b5bbf93768f099e2ee341783e4468368023e93124',
+    publicKey: '0xf08a5a3208d0f31ba71e',
+    balance: 50
+  },
+  {
+    privateKey: '2af989db1de25988b1aba8a3be2512a78cfb73e7b4aaef9237a52738ce19bc49',
+    publicKey: '0x016975d7079e08a08f4f',
+    balance: 25
+  },
+  {
+    privateKey: 'cf88b28b3a1d9785974fb1d5e7a63f7473c5817399ee17ec8995e51c550ffc4e',
+    publicKey: '0x6281ca944ead59f3b37e',
+    balance: 70
+  },
+  {
+    privateKey: '770f2bf3877e8d3bb75cd7d0d407e1e333d893a41f94080616c153ffb32f3ee6',
+    publicKey: '0xf6e1a0de29ac9fbbb8aa',
+    balance: 0
+  },
+]
+
+function hashMessage (message) {
+  return keccak256(utf8ToBytes(message))
+}
+
+async function signMessage (message, privateKey) {
+  const hash = hashMessage(message)
+  return await sign(hash, privateKey, { recovered: true })
+}
+
+function getBalanceFromAddress (address) {
+  for (let x = 0; x < wallets.length; x++) {
+    if (wallets[x].publicKey === address) {
+      return wallets[x].balance
+    }
+  }
+}
+ 
+
+app.get('/wallets', (req, res) => {
+  return res.send(wallets)
+})
+
+app.get('/balances', (req, res) => {
+  return res.send(balances)
+})
+
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
@@ -18,19 +70,26 @@ app.get("/balance/:address", (req, res) => {
   res.send({ balance });
 });
 
-app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+app.post("/send", async (req, res) => {
+  const { sender, recipient, amount, signature, recoveryBit } = req.body;
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+  console.log(signature)
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+  const recovered = await recoverPublicKey(hashMessage(`${recipient}${amount}`), signature, recoveryBit)
+
+  console.log(`Got recovered publicKey: ${recovered}`);
+
+  if (getBalanceFromAddress(recovered) >= amount) {
+
   }
+
+  // if (balances[sender] < amount) {
+  //   res.status(400).send({ message: "Not enough funds!" });
+  // } else {
+  //   balances[sender] -= amount;
+  //   balances[recipient] += amount;
+  //   res.send({ balance: balances[sender] });
+  // }
 });
 
 app.listen(port, () => {
